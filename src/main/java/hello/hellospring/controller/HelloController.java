d9109e4dd681da539f4d54243a2d1918460a6a26
sookjong <sookjong3367@naver.com>
2021-05-24 22:50:28 +0900

    객체를 반환하여 출력 (json 방식)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,4 +21,55 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+
+    @GetMapping("hello-string")
+    @ResponseBody         //html 태그 없이 문자가 그대로 화면에 출력됨
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+name;   //name=spring -->> "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public  Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); //Hello 객체 생성
+        hello.setName(name);
+        
+        return hello; //객체를 반환
+    }
+
+
+    static class Hello{
+
+        //getter and setter 
+        private String name;
+
+        public String getName() {  //꺼낼 때
+            return name;
+        }
+
+        public void setName(String name) { //넣을 때
+            this.name = name;
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
