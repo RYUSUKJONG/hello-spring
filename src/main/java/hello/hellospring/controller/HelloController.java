eaf56b4dfa10230847b5a0ad2b5272e6ac296f95
sookjong <sookjong3367@naver.com>
2021-05-24 22:21:03 +0900

    MVC, 파라미터를 통한 GET 방식 전송

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,21 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "hello!!");
+        model.addAttribute("data", "Spring!!");
         return "hello";
+    }
+
+
+    @GetMapping("hello-mvc")   //MVC 파라미터 이용하여 name 전달
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
